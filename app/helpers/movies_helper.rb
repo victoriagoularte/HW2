681d7c0a34e5da7fd7a34d87e78557e86eabaa40
Victória Goularte <victoriagr@aluno.unb.br>
2016-09-19 17:16:06 -0300

    Part 2. Adicionando checkbox para filtro.

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -20,4 +20,20 @@ module MoviesHelper
 		end 
 	end
 
+	def helper_select(movie)
+		if(params[:ratings] == nil)
+			return true
+		else
+			return params[:ratings].has_key?(movie.rating)
+		end
+	end
+
+	def helper_check(rating)
+		if(params[:ratings] == nil)
+			return false
+		end
+
+		return params[:ratings].has_key?(rating)
+	end
+
 end	
